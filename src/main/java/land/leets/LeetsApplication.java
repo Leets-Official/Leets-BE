5c0583e7c9e81b0b24988214dfc2c9d1db8f9963
ay-eonii <naayen@naver.com>
2023-07-25 23:24:23 +0900

    feat: Initial Project

---
+++ src/main/java/land/leets/LeetsApplication.java
@@ -0,0 +1,13 @@
+package land.leets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/land/leets/LeetsApplicationTests.java
@@ -0,0 +1,13 @@
+package land.leets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
