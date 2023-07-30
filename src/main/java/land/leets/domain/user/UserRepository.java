3a9924beedf7a0e7971ccb52d99031a8413bf5fa
ay-eonii <naayen@naver.com>
2023-07-30 15:19:47 +0900

    feat: 유저 repository 추가

---
+++ src/main/java/land/leets/domain/user/UserRepository.java
@@ -0,0 +1,23 @@
+package land.leets.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+
+    @Query("SELECT u.refreshToken FROM users u WHERE u.id=:id")
+    String getRefreshTokenById(@Param("id") Long id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE users u SET u.refreshToken=:token WHERE u.id=:id")
+    void updateRefreshToken(@Param("id") Long id, @Param("token") String token);
+}
