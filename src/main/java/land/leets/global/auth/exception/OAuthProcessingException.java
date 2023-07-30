e8d99cd12da6d8b75762565df38443461d35e384
ay-eonii <naayen@naver.com>
2023-07-30 15:19:24 +0900

    feat: 예외 추가

---
+++ src/main/java/land/leets/global/auth/exception/OAuthProcessingException.java
@@ -0,0 +1,8 @@
+package land.leets.global.auth.exception;
+
+public class OAuthProcessingException extends RuntimeException {
+
+    public OAuthProcessingException(String message) {
+        super(message);
+    }
+}
