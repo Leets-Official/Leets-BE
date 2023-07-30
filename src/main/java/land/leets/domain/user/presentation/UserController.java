f421b10226536d0f9084f959e848a4e895979e75
ay-eonii <naayen@naver.com>
2023-07-30 15:20:18 +0900

    feat: 유저 컨트롤러 추가

---
+++ src/main/java/land/leets/domain/user/presentation/UserController.java
@@ -0,0 +1,36 @@
+package land.leets.domain.user.presentation;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import land.leets.domain.user.AuthService;
+import land.leets.domain.user.User;
+import land.leets.domain.user.UserRepository;
+import land.leets.global.auth.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final AuthService authService;
+
+
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@AuthenticationPrincipal CustomUserDetails user) {
+        return userRepository.findById(user.getId()).orElseThrow(() -> new IllegalStateException("not found user"));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity refreshToken(HttpServletRequest request, HttpServletResponse response, @RequestBody String accessToken) {
+        return ResponseEntity.ok().body(authService.refreshToken(request, response, accessToken));
+    }
+}
