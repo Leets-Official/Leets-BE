74e138af5379e7bb338d50158503b4a94987def1
ay-eonii <naayen@naver.com>
2023-08-05 19:15:57 +0900

    feat: 관리자 정보 담는 인터페이스 추가

---
+++ src/main/java/land/leets/global/auth/CustomAdminDetails.java
@@ -0,0 +1,54 @@
+package land.leets.global.auth;
+
+import land.leets.domain.shared.AuthRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@AllArgsConstructor
+public class CustomAdminDetails implements UserDetails {
+    private final Long id;
+    private final String email;
+    private final AuthRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.getRole()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
