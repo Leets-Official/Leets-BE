be0bd66765ea8fe3d2d8c2be527f6af6b793fa77
ay-eonii <naayen@naver.com>
2023-08-05 17:37:13 +0900

    fix: JPA 변경

--- src/main/java/land/leets/domain/user/domain/repository/UserRepository.java
+++ src/main/java/land/leets/domain/user/domain/repository/UserRepository.java
@@ -8,9 +8,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findById(Long id);
 
