d2491b2ba65436a9779e634c1a027a8a926cce72
ay-eonii <naayen@naver.com>
2023-08-06 21:50:55 +0900

    feat: 로그인 시 토큰 발급

--- src/main/java/land/leets/domain/auth/presentation/AuthController.java
+++ src/main/java/land/leets/domain/auth/presentation/AuthController.java
@@ -1,11 +1,12 @@
 package land.leets.domain.auth.presentation;
 
 import land.leets.domain.auth.AuthService;
-import land.leets.domain.auth.presentation.dto.OAuthTokenDto;
+import land.leets.domain.shared.AuthRole;
 import land.leets.domain.user.domain.User;
+import land.leets.global.jwt.JwtProvider;
+import land.leets.global.jwt.dto.JwtResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +15,16 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtProvider jwtProvider;
+
 
     @GetMapping("/login/oauth2/callback/google")
-    public User successGoogleLogin(@RequestParam("code") String code) {
+    public JwtResponse successGoogleLogin(@RequestParam("code") String code) {
+        User user = authService.getGoogleAccessToken(code);
 
-        return authService.getGoogleAccessToken(code);
+        String accessToken = this.jwtProvider.generateToken(user.getUid(), user.getSub(), AuthRole.ROLE_USER, false);
+        String refreshToken = this.jwtProvider.generateToken(user.getUid(), user.getSub(), AuthRole.ROLE_USER, true);
+
+        return new JwtResponse(accessToken, refreshToken);
     }
 }
