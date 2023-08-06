ea72a78ce575ae498f098a551ddaa76fda3789ca
ay-eonii <naayen@naver.com>
2023-08-06 21:10:17 +0900

    chore: 불필요한 코드 삭제

--- src/main/java/land/leets/global/config/SecurityConfig.java
+++ src/main/java/land/leets/global/config/SecurityConfig.java
@@ -1,10 +1,6 @@
 package land.leets.global.config;
 
 import land.leets.domain.shared.AuthRole;
-import land.leets.global.auth.CustomOAuth2UserService;
-import land.leets.global.auth.OAuth2AuthenticationFailureHandler;
-import land.leets.global.auth.OAuth2AuthenticationSuccessHandler;
-import land.leets.global.auth.repository.CookieAuthorizationRequestRepository;
 import land.leets.global.filter.ExceptionHandleFilter;
 import land.leets.global.jwt.*;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -30,12 +21,6 @@ import java.util.List;
 public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
-    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -53,34 +38,23 @@ public class SecurityConfig {
 
         //요청에 대한 권한 설정
         http.authorizeHttpRequests()
+                .requestMatchers(CorsUtils::isCorsRequest).permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                 .requestMatchers("/oauth2/**", "/auth/**").permitAll()
-                .requestMatchers("/user/me").hasAnyAuthority(AuthRole.ROLE_USER.getRole())
+                .requestMatchers("/user/login", "/admin/login").permitAll()
+                .requestMatchers("/user/me").permitAll()
                 .requestMatchers("/login/user").hasAnyAuthority(AuthRole.ROLE_USER.getRole())
+                .requestMatchers("/login/oauth2/callback/*").permitAll()
+                .requestMatchers("/oauth2/authorization/*").permitAll()
+                .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated();
 
         //oauth2Login
-        http.oauth2Login()
-                .authorizationEndpoint()
-                .authorizationRequestRepository(cookieAuthorizationRequestRepository)
-                .and()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService)
-                .and()
-                .successHandler(oAuth2AuthenticationSuccessHandler)
-                .failureHandler(oAuth2AuthenticationFailureHandler);
-
-        http.logout()
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID");
+        http.oauth2Login();
 
         //jwt filter 설정
         http.addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new ExceptionHandleFilter(), JwtFilter.class);
-
-        http.exceptionHandling()
-                .accessDeniedHandler(jwtAccessDeniedHandler)
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint);
 
 
         return http.build();
