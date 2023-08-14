f5a5e926a8a8f90bb7c5a4b89027c58a6f45d2be
ay-eonii <naayen@naver.com>
2023-08-14 15:59:44 +0900

    fix: LoginRequest 어노테이션 변경

--- src/main/java/land/leets/domain/user/presentation/dto/LoginRequest.java
+++ src/main/java/land/leets/domain/user/presentation/dto/LoginRequest.java
@@ -1,10 +1,10 @@
 package land.leets.domain.user.presentation.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String idToken;
 }
