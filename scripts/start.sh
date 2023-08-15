a5c43ac8c3bc4b08b78da0414887b02d0204f04b
ay-eonii <naayen@naver.com>
2023-08-15 17:12:03 +0900

    fix: 배포 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,6 +12,8 @@ NOW=$(date +%c)
 echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/leets-0.0.1-SNAPSHOT.jar $JAR
 
+source .env
+
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
