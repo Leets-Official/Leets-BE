0e906f8b695e4ca7ba9b2605bbd9e15ae2130a00
ay-eonii <naayen@naver.com>
2023-08-17 21:09:51 +0900

    fix: 'UpdateUserImpl' 오류 수정

--- src/main/java/land/leets/domain/user/domain/repository/UserRepository.java
+++ src/main/java/land/leets/domain/user/domain/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findBySub(String sub);
+    Optional<User> findByUid(UUID uid);
 }
--- src/main/java/land/leets/domain/user/usecase/UpdateUserImpl.java
+++ src/main/java/land/leets/domain/user/usecase/UpdateUserImpl.java
@@ -3,6 +3,7 @@ package land.leets.domain.user.usecase;
 import land.leets.domain.application.presentation.dto.ApplicationRequest;
 import land.leets.domain.user.domain.User;
 import land.leets.domain.user.domain.repository.UserRepository;
+import land.leets.domain.user.exception.UserNotFoundException;
 import land.leets.domain.user.presentation.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class UpdateUserImpl implements UpdateUser {
     @Override
     public User execute(UUID uid, ApplicationRequest request) {
 
-        User user = userRepository.findById(uid).orElseThrow();
+        User user = userRepository.findByUid(uid).orElseThrow(UserNotFoundException::new);
         userMapper.updateUserFromDto(user, request);
 
         return userRepository.save(user);
