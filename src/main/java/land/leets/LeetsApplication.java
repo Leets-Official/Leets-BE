4fd33258623add8572fd4417aac6125ce38a9202
ay-eonii <naayen@naver.com>
2023-08-19 23:31:31 +0900

    feat: 메일 전송 cron 추가

--- src/main/java/land/leets/LeetsApplication.java
+++ src/main/java/land/leets/LeetsApplication.java
@@ -3,9 +3,11 @@ package land.leets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class LeetsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/land/leets/global/cron/SendMailCron.java
@@ -0,0 +1,29 @@
+package land.leets.global.cron;
+
+import land.leets.domain.mail.usercase.SendMail;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class SendMailCron {
+    private final SendMail sendMail;
+
+//    @Scheduled(cron = "0 0 10 4 9 ?")
+    @Scheduled(cron = "10 35 23 19 8 ?")
+    public void sendPaperMail() {
+        sendMail.execute("paper");
+
+        log.info("Send paper result mail successfully.");
+    }
+
+    @Scheduled(cron = "0 0 18 11 9 ?")
+    public void sendFinalMail() {
+        sendMail.execute("final");
+
+        log.info("Send final result mail successfully.");
+    }
+}
