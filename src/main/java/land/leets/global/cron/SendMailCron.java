30a68ef11959d2b45495b72016666f7599c6f1ec
A YEON <110026001+ay-eonii@users.noreply.github.com>
2023-08-21 02:00:45 +0900

    chore: 메일 cron 테스트

--- src/main/java/land/leets/global/cron/SendMailCron.java
+++ src/main/java/land/leets/global/cron/SendMailCron.java
@@ -12,15 +12,15 @@ import org.springframework.stereotype.Component;
 public class SendMailCron {
     private final SendMail sendMail;
 
-//    @Scheduled(cron = "0 0 10 4 9 ?")
-    @Scheduled(cron = "0 51 1 21 8 ?")
+    @Scheduled(cron = "0 0 10 4 9 ?")
     public void sendPaperMail() {
         sendMail.execute("paper");
 
         log.info("Send paper result mail successfully.");
     }
 
-    @Scheduled(cron = "0 0 18 11 9 ?")
+    // @Scheduled(cron = "0 0 18 11 9 ?")
+    @Scheduled(cron = "0 3 2 21 8 ?")
     public void sendFinalMail() {
         sendMail.execute("final");
 
