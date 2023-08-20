8a4a0817cdb4ef03eb00da8d3436cbd8f6862e76
ay-eonii <naayen@naver.com>
2023-08-20 23:12:06 +0900

    chore: send mail cron test

--- src/main/java/land/leets/global/cron/SendMailCron.java
+++ src/main/java/land/leets/global/cron/SendMailCron.java
@@ -13,7 +13,7 @@ public class SendMailCron {
     private final SendMail sendMail;
 
 //    @Scheduled(cron = "0 0 10 4 9 ?")
-    @Scheduled(cron = "0 42 22 20 8 ?")
+    @Scheduled(cron = "0 15 23 20 8 ?")
     public void sendPaperMail() {
         sendMail.execute("paper");
 
