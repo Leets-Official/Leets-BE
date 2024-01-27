6157cbaf0482002b1e02d7bb963d20e880e4dc2e
ay-eonii <naayen@naver.com>
2024-01-27 19:52:39 +0900

    feat: Comment 조회

--- src/main/java/land/leets/domain/admin/presentation/dto/AdminDetailsResponse.java
+++ src/main/java/land/leets/domain/admin/presentation/dto/AdminDetailsResponse.java
@@ -1,10 +1,12 @@
 package land.leets.domain.admin.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class AdminDetailsResponse {
+    @NotBlank
     private String name;
 }
---
+++ src/main/java/land/leets/domain/comment/presentation/dto/CommentResponse.java
@@ -0,0 +1,25 @@
+package land.leets.domain.comment.presentation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import land.leets.domain.admin.presentation.dto.AdminDetailsResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class CommentResponse {
+    @NotBlank
+    private String content;
+
+    @NotBlank
+    private LocalDateTime createdAt;
+
+    @NotBlank
+    private LocalDateTime updatedAt;
+
+    @NotNull
+    private AdminDetailsResponse admin;
+}
