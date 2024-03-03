856ec1cdb8fdedc2efa5c897e693605b16ad054b
ay-eonii <naayen@naver.com>
2024-03-03 23:05:58 +0900

    !HOTFIX: 로그인 요청 생성자 추가

--- src/main/java/land/leets/domain/user/presentation/dto/LoginRequest.java
+++ src/main/java/land/leets/domain/user/presentation/dto/LoginRequest.java
@@ -2,9 +2,11 @@ package land.leets.domain.user.presentation.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String idToken;
 }
