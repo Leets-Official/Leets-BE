c2a09af97b0bfa1a95f4179432ecfeb87a78bb8c
ay-eonii <naayen@naver.com>
2024-03-18 00:42:47 +0900

    !HOTFIX: 메일 cron 수정

--- src/main/java/land/leets/global/cron/SendMailCron.java
+++ src/main/java/land/leets/global/cron/SendMailCron.java
@@ -3,6 +3,7 @@ package land.leets.global.cron;
 import land.leets.domain.mail.usecase.SendMail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -11,21 +12,21 @@ import org.springframework.stereotype.Component;
 public class SendMailCron {
     private final SendMail sendMail;
 
-//    @Scheduled(cron = "0 0 10 9 9 ?")
+    @Scheduled(cron = "0 0 10 19 3 ?")
     public void sendPaperMail() {
         sendMail.execute("paper");
 
         log.info("Send paper result mail successfully.");
     }
 
-//     @Scheduled(cron = "0 0 18 18 9 ?")
+    @Scheduled(cron = "0 0 18 27 3 ?")
     public void sendFinalMail() {
         sendMail.execute("final");
 
         log.info("Send final result mail successfully.");
     }
 
-//     @Scheduled(cron = "0 28 23 1 9 ?")
+    //     @Scheduled(cron = "0 28 23 1 9 ?")
     public void sendPlusMail() {
         sendMail.execute("plus");
 
