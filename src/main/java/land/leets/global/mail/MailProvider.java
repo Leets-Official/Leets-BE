47457b18559bfdcfdf36c792c79f9c8b4c44563a
ay-eonii <naayen@naver.com>
2024-08-04 15:26:37 +0900

    feat: 메일 병렬처리 추가

--- src/main/java/land/leets/global/mail/MailProvider.java
+++ src/main/java/land/leets/global/mail/MailProvider.java
@@ -1,55 +1,54 @@
 package land.leets.global.mail;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import land.leets.global.mail.dto.MailDto;
+import land.leets.global.mail.exception.MailException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Component;
-
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import land.leets.global.mail.dto.MailDto;
-import lombok.RequiredArgsConstructor;
-
 @Component
 @RequiredArgsConstructor
 public class MailProvider {
 
-	private static final int THREAD_POOL_SIZE = 10;
+    private static final int THREAD_POOL_SIZE = 4;
 
-	private final JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
 
-	public void sendEmails(List<MailDto> mailDtos) {
-		ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+    public void sendEmails(List<MailDto> mailDtos) {
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-		for (MailDto mail : mailDtos) {
-			executor.submit(() -> sendEmail(mail));
-		}
+        mailDtos.forEach(mail -> executor.submit(() -> sendEmail(mail)));
 
-		executor.shutdown();
-		try {
-			if (!executor.awaitTermination(120, TimeUnit.SECONDS)) {
-				executor.shutdownNow();
-			}
-		} catch (InterruptedException e) {
-			executor.shutdownNow();
-			Thread.currentThread().interrupt();
-		}
-	}
+        executor.shutdown();
 
-	private void sendEmail(MailDto mailDto) {
-		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-		try {
-			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-			mimeMessageHelper.setTo(mailDto.to());
-			mimeMessageHelper.setSubject(mailDto.title());
-			mimeMessageHelper.setText(mailDto.body(), true);
-			javaMailSender.send(mimeMessage);
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void sendEmail(MailDto mailDto) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(mailDto.to());
+            mimeMessageHelper.setSubject(mailDto.title());
+            mimeMessageHelper.setText(mailDto.body(), true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new MailException();
+        }
+    }
 }
