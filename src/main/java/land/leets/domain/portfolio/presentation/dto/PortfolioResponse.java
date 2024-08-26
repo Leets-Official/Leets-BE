f2cbd6099b4dfc6dfafa0a07133bddfb8e6ba30b
노정완 <jeongwan.noh@gmail.com>
2024-08-26 19:19:55 +0900

    !HOTFIX: 프로젝트 조회 DTO 수정

--- src/main/java/land/leets/domain/portfolio/presentation/dto/PortfolioResponse.java
+++ src/main/java/land/leets/domain/portfolio/presentation/dto/PortfolioResponse.java
@@ -1,6 +1,5 @@
 package land.leets.domain.portfolio.presentation.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import land.leets.domain.contributor.domain.Contributor;
 import land.leets.domain.portfolio.domain.ProjectScope;
 import land.leets.domain.portfolio.domain.ProjectType;
@@ -14,42 +13,29 @@ import java.util.List;
 @AllArgsConstructor
 public class PortfolioResponse {
 
-    @NotBlank
     private Long portfolioId;
 
-    @NotBlank
     private Long generation;
 
-    @NotBlank
     private String name;
 
-    @NotBlank
     private String summary;
 
-    @NotBlank
     private String description;
 
-    @NotBlank
     private ProjectType type;
 
-    @NotBlank
     private ProjectScope scope;
 
-    @NotBlank
     private LocalDate startDate;
 
-    @NotBlank
     private LocalDate endDate;
 
-    @NotBlank
     private String serviceUrl;
 
-    @NotBlank
     private String logoImgName;
 
-    @NotBlank
     private String mainImgName;
 
-    @NotBlank
     private List<Contributor> contributors;
 }
