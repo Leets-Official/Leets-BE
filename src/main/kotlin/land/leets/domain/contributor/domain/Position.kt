3594996d3c57217b0200e5437d111f37bcaaef83
rootTiket <l20078829@gmail.com>
2025-12-19 21:14:00 +0900

    feat: position 마이그레이션

---
+++ src/main/kotlin/land/leets/domain/contributor/domain/Position.kt
@@ -0,0 +1,8 @@
+package land.leets.domain.contributor.domain
+
+enum class Position(val position: String) {
+    BACK_END("BackEnd"),
+    FRONT_END("FrontEnd"),
+    BX_BI("BX/BI"),
+    UX_UI("UX/UI")
+}
