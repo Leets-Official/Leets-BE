83128def55f030f60d0f4d3193af784613dd824e
rootTiket <l20078829@gmail.com>
2025-12-19 21:14:47 +0900

    refactor: 기존 로직 삭제

--- src/main/java/land/leets/domain/contributor/domain/repository/ContributorRepository.java
+++
@@ -1,7 +0,0 @@
-package land.leets.domain.contributor.domain.repository;
-
-import land.leets.domain.contributor.domain.Contributor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContributorRepository extends JpaRepository<Contributor, Long> {
-}
