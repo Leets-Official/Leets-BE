3b4f76cacf31577568adcf183ab2462777e645e8
rootTiket <l20078829@gmail.com>
2025-12-22 22:10:43 +0900

    refactor: 기존 로직 삭제

--- src/main/java/land/leets/domain/user/usecase/GetUserDetails.java
+++
@@ -1,9 +0,0 @@
-package land.leets.domain.user.usecase;
-
-import land.leets.domain.auth.AuthDetails;
-import land.leets.domain.user.presentation.dto.UserDetailsResponse;
-
-public interface GetUserDetails {
-
-    UserDetailsResponse execute(AuthDetails authDetails);
-}
