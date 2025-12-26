0e6e6432363f6f7275c3c13e11d349ab72d65d30
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-12-26 17:50:44 +0900

    refactor: 예외 처리 kotlin 마이그레이션에 따른 코드 수정

--- src/main/kotlin/land/leets/global/filter/ExceptionHandleFilter.kt
+++ src/main/kotlin/land/leets/global/filter/ExceptionHandleFilter.kt
@@ -22,7 +22,7 @@ class ExceptionHandleFilter : OncePerRequestFilter() {
             filterChain.doFilter(request, response)
         }.onFailure { exception ->
             val errorCode = when (exception) {
-                is ServiceException -> exception.getErrorCode() // TODO: ServiceException을 Kotlin으로 마이그레이션 후 get 메서드 제거
+                is ServiceException -> exception.errorCode
                 else -> ErrorCode.INTERNAL_SERVER_ERROR
             }
             sendErrorResponse(response, errorCode)
@@ -32,11 +32,11 @@ class ExceptionHandleFilter : OncePerRequestFilter() {
     @Throws(IOException::class)
     private fun sendErrorResponse(response: HttpServletResponse, errorCode: ErrorCode) {
         response.apply {
-            status = errorCode.getHttpStatus() // TODO: ErrorCode를 Kotlin으로 마이그레이션 후 get 메서드 제거
+            status = errorCode.httpStatus
             characterEncoding = "UTF-8"
             contentType = "application/json"
 
-            val errorResponse = ErrorResponse.of(errorCode)
+            val errorResponse = ErrorResponse.from(errorCode)
             val result = mapOf("result" to errorResponse)
 
             writer.write(ObjectMapper().writeValueAsString(result))
