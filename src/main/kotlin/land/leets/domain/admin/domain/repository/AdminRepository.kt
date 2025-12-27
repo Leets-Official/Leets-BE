e0187e9bcad50e5472eff06d33beb54580d0a9e2
Jeongwan Noh <jwnnoh@gmail.com>
2025-12-27 16:22:42 +0900

    refactor: Admin, User 도메인 Optional 사용 제거

--- src/main/kotlin/land/leets/domain/admin/domain/repository/AdminRepository.kt
+++ src/main/kotlin/land/leets/domain/admin/domain/repository/AdminRepository.kt
@@ -2,9 +2,8 @@ package land.leets.domain.admin.domain.repository
 
 import land.leets.domain.admin.domain.Admin
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
 import java.util.UUID
 
 interface AdminRepository : JpaRepository<Admin, UUID> {
-    fun findByUsername(username: String): Optional<Admin>
+    fun findByUsername(username: String): Admin?
 }
