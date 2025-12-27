34c70540a665f3a00a238fdf015312085ada2e26
Jeongwan Noh <jwnnoh@gmail.com>
2025-12-27 16:25:17 +0900

    refactor: OAuth 로직 마이그레이션

---
+++ src/main/kotlin/land/leets/domain/auth/AuthService.kt
@@ -0,0 +1,69 @@
+package land.leets.domain.auth
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.gson.GsonFactory
+import land.leets.domain.auth.exception.PermissionDeniedException
+import land.leets.domain.auth.presentation.dto.OAuthTokenDto
+import land.leets.domain.user.domain.User
+import land.leets.domain.user.domain.repository.UserRepository
+import land.leets.global.jwt.exception.InvalidTokenException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+
+@Service
+class AuthService(
+    @Value("\${google.auth.url}") private val googleAuthUrl: String,
+    @Value("\${google.redirect.url}") private val googleRedirectUrl: String,
+    @Value("\${spring.security.oauth2.client.registration.google.client-id}") private val googleClientId: String,
+    @Value("\${spring.security.oauth2.client.registration.google.client-secret}") private val googleClientPassword: String,
+    private val userRepository: UserRepository
+) {
+
+    fun getGoogleToken(code: String): User {
+        val restTemplate = RestTemplate()
+        val params = mapOf(
+            "code" to code,
+            "client_id" to googleClientId,
+            "client_secret" to googleClientPassword,
+            "redirect_uri" to googleRedirectUrl,
+            "grant_type" to "authorization_code"
+        )
+
+        val responseEntity = restTemplate.postForEntity(googleAuthUrl, params, OAuthTokenDto::class.java)
+
+        if (responseEntity.statusCode != HttpStatus.OK || responseEntity.body == null) {
+            throw PermissionDeniedException()
+        }
+
+        val idToken = responseEntity.body!!.idToken
+        return getUser(idToken)
+    }
+
+    fun getUser(idToken: String): User {
+        val verifier = GoogleIdTokenVerifier.Builder(NetHttpTransport(), GsonFactory.getDefaultInstance())
+            .setAudience(listOf(googleClientId))
+            .build()
+
+        val googleIdToken = try {
+            verifier.verify(idToken)
+        } catch (e: Exception) {
+            null
+        } ?: throw InvalidTokenException()
+
+        val payload = googleIdToken.payload
+        val userId = payload.subject
+
+        return userRepository.findBySub(userId) ?: userRepository.save(
+            User(
+                sid = null,
+                name = payload["name"].toString(),
+                phone = null,
+                email = payload.email,
+                sub = userId
+            )
+        )
+    }
+}
