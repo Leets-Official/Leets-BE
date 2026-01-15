fa3f5b5d6a346ec768ae1e4c325ea07da3af8993
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-01-15 17:35:51 +0900

    feat: 백엔드, 프론트엔드 직군 추가
    - 기존 개발 직군은 호환성을 위해 유지

--- src/main/kotlin/land/leets/domain/application/type/Position.kt
+++ src/main/kotlin/land/leets/domain/application/type/Position.kt
@@ -2,6 +2,8 @@ package land.leets.domain.application.type
 
 enum class Position(val statusKo: String) {
     DEV("개발"),
+    FRONTEND("프론트엔드"),
+    BACKEND("백엔드"),
     DESIGN("디자인"),
     BX_BI("BX/BI"),
     UX_UI("UX/UI"),
